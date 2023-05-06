43093935e5ed5aa0d73b44d0a4ef522582ed9c31
aganbake <abdul.villarroel@gmail.com>
2023-05-06 02:18:13 -0400

    latest release

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,33 +1,100 @@
-const defaultBudget = 0;
-let expenses = [];
-let expensesAmounts = [];
+let totalExpended = 0;
+let budget = 0;
+let fill = 100;
 
 const add = () => {
-  let budget = document.getElementById("budget").value;
+  budget = parseInt(document.getElementById("budget").value);
+  let userExpenses = document.getElementById("userExpenses").innerHTML;
+  let userRemanent = document.getElementById("userRemanent").innerHTML;
+  let gastos = userExpenses.replace(
+    /[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi,
+    ""
+  );
+  let saldos = userRemanent.replace(
+    /[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi,
+    ""
+  );
+  if (typeof budget == "undefined" || isNaN(budget)) {
+    budget = 0;
+  }
 
-  if (budget === null) {
-    budget = defaultBudget;
+  if (gastos === "" && saldos === "") {
+    document.getElementById("userExpenses").innerHTML = "$0";
+    document.getElementById("userRemanent").innerHTML = "$0";
+  }
+
+  if (saldos > 0) {
+    document.getElementById("userRemanent").innerHTML = `$${budget - gastos}`;
   }
 
   document.getElementById("userBudget").innerHTML = `$${budget}`;
+
+  if (budget > 0) {
+    document.querySelector(".btn-danger").removeAttribute("hidden");
+  }
+
+  document.getElementById("budget").value = "";
+  document.getElementById("circle").style.height = `${fill}%`;
+
+  $(".collpase").collapse("toggle");
 };
 
 const expense = () => {
   let expense = document.getElementById("expense").value;
   let expenseAmount = parseInt(document.getElementById("expenseAmount").value);
 
-  expenses.push(expense);
-  expensesAmounts.push(expenseAmount);
+  if (isNaN(expenseAmount)) {
+    alert("Ningún gasto");
+  } else {
+    let table = document.getElementById("tbody");
+    let row = document.createElement("tr");
+    row.innerHTML = `
+    <td>${expense}</td>
+    <td>${expenseAmount}</td>
+    <td><button class="btn" onclick="deleteItem(this)"><i class="bi bi-trash"></i></button></td>
+    `;
+    table.append(row);
+    update(parseInt(expenseAmount));
+  }
+  document.getElementById("expense").value = "";
+  document.getElementById("expenseAmount").value = "";
 
-  let table = document.getElementById("tbody");
-  let newRow = document.createElement("tr");
-  table.appendChild(newRow);
-  let newExpense = document.createElement("td");
-  let newAmount = document.createElement("td");
-  newRow.appendChild(newExpense);
-  newRow.appendChild(newAmount);
-  newExpense.innerHTML = expense;
-  newAmount.innerHTML = expenseAmount;
+  let prc = (expenseAmount / budget) * 100;
+  fill = fill - prc;
+  document.getElementById("circle").style.height = `${fill}%`;
 };
 
-const update = () => {};
+const update = (amount) => {
+  let userBudget = budget;
+
+  totalExpended += amount;
+  let newBudget = userBudget - totalExpended;
+
+  document.getElementById("userExpenses").innerHTML = `$${totalExpended}`;
+  document.getElementById("userRemanent").innerHTML = `$${newBudget}`;
+};
+
+const revert = (amount) => {
+  let userRemanent = document.getElementById("userRemanent").innerHTML;
+  userBudget = userRemanent.replace(
+    /[`~!@#$%^&*()_|+?;:'",.<>\{\}\[\]\\\/]/gi,
+    ""
+  );
+
+  totalExpended = totalExpended - amount;
+  let newBudget = parseInt(userBudget) + amount;
+  document.getElementById("userExpenses").innerHTML = `$${totalExpended}`;
+  document.getElementById("userRemanent").innerHTML = `$${newBudget}`;
+
+  let prc = (newBudget / budget) * 100;
+  fill = prc;
+  document.getElementById("circle").style.height = `${fill}%`;
+};
+
+const deleteItem = (row) => {
+  let thisAmount = row.parentNode.parentNode.childNodes[3].innerText;
+  revert(parseInt(thisAmount));
+
+  let thisRow = row.parentNode.parentNode.rowIndex;
+  document.getElementById("tbody").deleteRow(thisRow);
+};
